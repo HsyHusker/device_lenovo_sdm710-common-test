19796f6e62ce8522021ccf4952758f20a6ab6645
Husker <misterhusker19@gmail.com>
2022-09-14 09:28:38 +0800

    sdm710-common: update build fp and desc to Sept raven 13

--- init/init_sdm710.cpp
+++ init/init_sdm710.cpp
@@ -34,16 +34,16 @@ void property_override(char const prop[], char const value[]) {
 }
 
 void vendor_load_properties() {
-    property_override("ro.build.description", "raven-user 12 SQ1D.220205.003 8069835 release-keys");
-    property_override("ro.vendor.build.description", "raven-user 12 SQ1D.220205.003 8069835 release-keys");
-    property_override("ro.system.build.description", "raven-user 12 SQ1D.220205.003 8069835 release-keys");
-    property_override("ro.bootimage.build.description", "raven-user 12 SQ1D.220205.003 8069835 release-keys");
-    property_override("ro.product.build.description", "raven-user 12 SQ1D.220205.003 8069835 release-keys");
-    property_override("ro.build.fingerprint", "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys");
-    property_override("ro.vendor.build.fingerprint", "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys");
-    property_override("ro.bootimage.build.fingerprint", "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys");
-    property_override("ro.system.build.fingerprint", "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys");
-    property_override("ro.product.build.fingerprint", "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys");
+    property_override("ro.build.description", "raven-user 13 TP1A.220905.004 8927612 release-keys");
+    property_override("ro.vendor.build.description", "raven-user 13 TP1A.220905.004 8927612 release-keys");
+    property_override("ro.system.build.description", "raven-user 13 TP1A.220905.004 8927612 release-keys");
+    property_override("ro.bootimage.build.description", "raven-user 13 TP1A.220905.004 8927612 release-keys");
+    property_override("ro.product.build.description", "raven-user 13 TP1A.220905.004 8927612 release-keys");
+    property_override("ro.build.fingerprint", "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys");
+    property_override("ro.vendor.build.fingerprint", "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys");
+    property_override("ro.bootimage.build.fingerprint", "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys");
+    property_override("ro.system.build.fingerprint", "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys");
+    property_override("ro.product.build.fingerprint", "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys");
     property_override("ro.oem_unlock_supported", "0");
 
 }
